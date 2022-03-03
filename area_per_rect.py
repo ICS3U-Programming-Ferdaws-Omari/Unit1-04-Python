01a032c1fad47e993bca6c8fc964bd41163613c0
Ubuntu <ubuntu@ip-172-31-85-213.ec2.internal>
2022-03-03 02:43:49 +0000

    math

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created By: Ferdaws
+# Date: 03/01/2022
+# This program shows a question and the solution to it.
+# The question is about the area and perimeter of a rectangle. 
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("10cm x 5cm")
+    print("")
+    print("Area is {}cm^2".format(10*5))
+    print("perimeter is {}cm".format(2*(10+5)))
+    
+    
+if __name__ == "__main__": 
+    main()
---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Ferdaws
+# Date: 03/01/2022
+# This program runs math operations.
+import math 
+
+def main():
+#This function does basic math.
+    print("7+8={}".format(7+8))
+    print("9-6={}".format(9-6))
+    print("4*5={}".format(4*5))
+    print("8/3={}".format(8/3))
+    print("4**2={}".format(4**2))
+    print(math.sqrt(9))
+    
+if __name__ == "__main__": 
+    main()
